644ae2725bb69b52db969d44ff0fc74fa5841c02
ZaxarShashkov <zekharjah@gmail.com>
2023-08-29 22:12:29 +0300

    upd

--- src/components/UI/Count/Count.tsx
+++ src/components/UI/Count/Count.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import styles from './Count.module.scss';
 
 type CountProps = {
 	count: number | string;
 };
 
-const Count = ({ count }: CountProps) => {
+const Count = ({ count }: CountProps): ReactNode => {
 	return (
 		<>
 			<p className={styles.count}>{count}</p>
